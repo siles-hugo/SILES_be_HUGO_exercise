98912b9f78f06718aac962c3847819e0adeebeda
Hugo Siles <hugo.siles.noreply@gmail.com>
2023-05-22 09:56:17 -0400

    ECO-003 Clean magic numbers for http status code

--- src/main/java/com/ecore/roles/web/rest/UsersRestController.java
+++ src/main/java/com/ecore/roles/web/rest/UsersRestController.java
@@ -4,10 +4,10 @@ import com.ecore.roles.service.UsersService;
 import com.ecore.roles.web.UsersApi;
 import com.ecore.roles.web.dto.UserDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +29,7 @@ public class UsersRestController implements UsersApi {
             produces = {"application/json"})
     public ResponseEntity<List<UserDto>> getUsers() {
         return ResponseEntity
-                .status(200)
+                .status(HttpStatus.OK)
                 .body(usersService.getUsers().stream()
                         .map(UserDto::fromModel)
                         .collect(Collectors.toList()));
@@ -42,7 +42,7 @@ public class UsersRestController implements UsersApi {
     public ResponseEntity<UserDto> getUser(
             @PathVariable UUID userId) {
         return ResponseEntity
-                .status(200)
+                .status(HttpStatus.OK)
                 .body(fromModel(usersService.getUser(userId)));
     }
 }
